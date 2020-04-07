5ba7a6030819d2ac4e1a3c8f813561d56305d75c
yonngNessa14 <nessa4u1@gmail.com>
2020-04-07 12:51:33 +0100

    cors iss

--- server.js
+++ server.js
@@ -12,6 +12,14 @@ require('./services/passport');
 const app = express();
 app.use(express.json());
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    
+    next();
+})
 app.use(cors());
 
 mongoose.connect(keys.mongoDbKey, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
