08aa9d136c948b9a19e0b16e24dfd5016127f9dd
yonngNessa14 <nessa4u1@gmail.com>
2020-04-07 12:34:33 +0100

    prod keys

---
+++ config/keys.js
@@ -0,0 +1,6 @@
+if (process.env.NODE_ENV === 'production') {
+    module.exports = require('./prods');
+}
+else {
+    module.exports = require('./dev');
+}
---
+++ config/prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+    facebookClientID: process.env.FACEBOOK_CLIENT_ID,
+    facebookClientSecret: process.env.FACEBOOK_CLIENT_SECRET,
+    mongoDbKey: process.env.MONGO_DB_KEY,
+    jwtKey: process.env.JWT_KEY,
+}
